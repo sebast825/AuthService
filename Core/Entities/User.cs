84b174f61b7e18ddc0424244d956253cc7fe4773
sebast825 <sebamolina825@gmail.com>
2025-10-15 22:34:27 -0300

    user

---
+++ Core/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class User : ClassBase
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+        public string? FullName { get; set; };
+    }
+}
