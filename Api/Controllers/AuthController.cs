9bed8521431ede1ac52f3c2ffb3cd11865e1a9be
sebast825 <sebamolina825@gmail.com>
2025-10-16 18:33:38 -0300

    ValidateCredentialsAsync

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Aplication.Services;
+using Core.Dto.Auth;
+using Core.Interfaces.Services;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        private readonly UserServicesI _userServiceI;
+        public AuthController(UserServicesI userServiceI)
         {
-            return View();
+            _userServiceI = userServiceI;
         }
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginRequestDto loginDto)
+        {
+            await _userServiceI.ValidateCredentialsAsync(loginDto);
+            return Ok();
+        }
+
+        /* [HttpPost("logout")]
+         public async Task<IActionResult> Logout()
+
+
+         [HttpPost("refresh-token")]
+             public async Task<IActionResult> RefreshToken(RefreshTokenRequest request)
+
+
+         [HttpPost("forgot-password")]
+             public async Task<IActionResult> ForgotPassword(ForgotPasswordRequest request)
+        */
     }
 }
