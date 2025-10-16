050b65eeb7941d9df10703e523de097fad7c66bc
sebast825 <sebamolina825@gmail.com>
2025-10-15 22:46:32 -0300

    Create RefreshToken.cs

---
+++ Core/Entities/RefreshToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class RefreshToken : ClassBase
+    {
+        public int UserId { get; set; }
+        public string Token { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+        public bool Revoked { get; set; }
+
+        public User User { get; set; } = null!;
+    }
+}
