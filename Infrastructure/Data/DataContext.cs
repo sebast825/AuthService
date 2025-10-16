0dae580f4a1ac24c5c61c06ed6ab59a24f10506d
sebast825 <sebamolina825@gmail.com>
2025-10-16 13:31:49 -0300

    DataContext

---
+++ Infrastructure/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options) { }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<LoginAttempt> LoginAttempts { get; set; } = null!;
+        public DbSet<RefreshToken>RefreshTokens { get; set; } = null!;
+    }
+}
