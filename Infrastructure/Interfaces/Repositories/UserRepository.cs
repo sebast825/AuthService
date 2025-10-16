9eb0953397fc50b7d56af4ff52f30770a9b0dec7
sebast825 <sebamolina825@gmail.com>
2025-10-16 15:27:01 -0300

    user repository

---
+++ Infrastructure/Interfaces/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Core.Interfaces.Repositories;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Interfaces.Repositories
+{
+    public class UserRepository : UserRepositoryI
+    {
+        private readonly DataContext _dataContext;
+        public UserRepository(DataContext dataContext) {
+            _dataContext = dataContext;
+        }
+        public async Task AddAsync(string email, string password, string? fullname = null)
+        {
+            User user = new User { Email = email, Password = password, FullName = fullname };
+            await _dataContext.Set<User>().AddAsync(user);
+        }
+    }
+}
