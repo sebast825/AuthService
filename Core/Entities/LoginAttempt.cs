5fdf81173c59502e366fd460b6adcdd3672014ce
sebast825 <sebamolina825@gmail.com>
2025-10-15 22:36:21 -0300

    LoginAttempt

---
+++ Core/Entities/LoginAttempt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class LoginAttempt : ClassBase
+    {
+        public int UserId { get; set; }
+        public bool Success { get; set; }
+        public string IpAddress { get; set; } = null!;
+        public User User { get; set; } = null!;
+    }
+}
