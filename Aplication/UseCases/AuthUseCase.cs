c2562ac7256d67c51b78b5b014eb2ea90dcf34e5
sebast825 <sebamolina825@gmail.com>
2025-10-17 18:30:53 -0300

    AuthUseCase

---
+++ Aplication/UseCases/AuthUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Aplication.Services;
+using Core.Dto.Auth;
+using Core.Entities;
+using Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.UseCases
+{
+    public class AuthUseCase
+    {
+        private readonly UserServicesI _userServicesI;
+        public AuthUseCase(UserServicesI userServicesI) {
+            _userServicesI = userServicesI;
+        }
+
+        public async Task Auth(LoginRequestDto loginDto)
+        {
+            _userServicesI.ValidateCredentialsAsync(loginDto);
+            throw new NotImplementedException();
+        }
+    }
+}
