cdb8dc33d8624aa2c1e26f03b71d8e2cbfdc5b94
sebast825 <sebamolina825@gmail.com>
2025-10-17 18:36:37 -0300

    JwtService

---
+++ Aplication/Services/JwtService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Services
+{
+    public class JwtService : JwtServiceI
+    {
+        public string GenerateAccessToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
