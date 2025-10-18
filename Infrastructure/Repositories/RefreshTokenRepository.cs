fab1f7e4c2654a5a0afaf243bc6e56b268764d4f
sebast825 <sebamolina825@gmail.com>
2025-10-18 15:13:11 -0300

    RefreshTokenRepository

---
+++ Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Core.Interfaces.Repositories;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class RefreshTokenRepository : RefreshTokenRepositoryI
+    {
+        private readonly DataContext _dataContext;
+        public RefreshTokenRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task AddAsync(RefreshToken token)
+        {
+            await _dataContext.Set<RefreshToken>().AddAsync(token);
+            await _dataContext.SaveChangesAsync();  
+        }
+    }
+}
