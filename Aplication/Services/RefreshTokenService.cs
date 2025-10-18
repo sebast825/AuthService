09e056b567485de6cd65a52d75a4dc08830c36ff
sebast825 <sebamolina825@gmail.com>
2025-10-17 23:05:30 -0300

    create refresh Token

---
+++ Aplication/Services/RefreshTokenService.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Core.GeoJson;
+using Core.Entities;
+using Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Services
+{
+    public class RefreshTokenService : RefreshTokenServiceI
+    {
+        public RefreshToken Create(int userId)
+        {
+           
+
+            RefreshToken refreshToken = new RefreshToken
+            {
+                UserId = userId,
+                Token = GenerateRefreshToken(),
+                ExpiresAt = GenerateExpirationDate()
+            };
+            return refreshToken;
+        
+        }
+
+
+        private string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+        private DateTime GenerateExpirationDate()
+        {
+            return DateTime.UtcNow.AddDays(1);
+        }
+    }
+}
