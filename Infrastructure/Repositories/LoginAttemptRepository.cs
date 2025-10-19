f01811b59a2e1fbfe58e37d1494815ad5368203f
sebast825 <sebamolina825@gmail.com>
2025-10-19 13:15:28 -0300

    LoginAttemptRepository

---
+++ Infrastructure/Repositories/LoginAttemptRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Core.Interfaces.Repositories;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class LoginAttemptRepository : LoginAttemptRepositoryI
+    {
+        private readonly DataContext _dataContext;
+        public LoginAttemptRepository(DataContext dataContext) {
+            _dataContext = dataContext;
+        } 
+        public async Task AddAsync(LoginAttempt loginAttempt)
+        {
+            await _dataContext.Set<LoginAttempt>().AddAsync(loginAttempt);
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
