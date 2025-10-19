13e91999383deb2f7b1ac6cb3ef0699b3e648f89
sebast825 <sebamolina825@gmail.com>
2025-10-18 22:13:33 -0300

    Create LoginAttemptsServiceI.cs

---
+++ Core/Interfaces/Services/LoginAttemptsServiceI.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces.Services
+{
+    public interface LoginAttemptsServiceI
+    {
+        Task AddAsync(LoginAttempt loginAttempt);
+        Task CreateLoginAttempt(int userId, string ip, bool success);
+    }
+}
