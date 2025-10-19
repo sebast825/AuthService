7e33da65424a2987011d44cd72d6fbd4c12c61c5
sebast825 <sebamolina825@gmail.com>
2025-10-18 21:44:49 -0300

    LoginAttemptsCacheService

---
+++ Aplication/Services/LoginAttemptsCacheService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Services
+{
+    public class LoginAttemptsCacheService
+    {
+        private readonly IMemoryCache _cache;
+        //range time until the cache restart
+        private readonly TimeSpan _window = TimeSpan.FromMinutes(5);
+        private readonly int _userLimit = 5;
+        private readonly int _ipLimit = 30;
+        public LoginAttemptsCacheService(IMemoryCache cache) {
+        _cache = cache;
+        }
+
+
+        public bool IsBlocked(string email, string ip)
+        {
+
+            return EmailIsBlocked(email) && IpIsBlocked(ip);
+
+        }
+
+        private bool EmailIsBlocked(string emailKey)
+        {
+            var emailAttempts = this.GetOrCreateLimit(emailKey);
+            return emailAttempts >= _userLimit;
+        }
+        private bool IpIsBlocked(string ipKey)
+        {
+            var ipAttempts = this.GetOrCreateLimit(ipKey);
+            return ipAttempts >= _ipLimit;
+        }
+        private int GetOrCreateLimit(string key)
+        {
+            int attempts = _cache.GetOrCreate(key, e =>
+            {
+                e.AbsoluteExpirationRelativeToNow = _window;
+                return 0;
+            });
+            return attempts; 
+        }
+    }
+}
