e25cd6e5adacf7af8755eca6c5089c0585039fc7
sebast825 <sebamolina825@gmail.com>
2025-10-19 22:23:34 -0300

    Create SecurityLoginAttempt.cs

---
+++ Core/Entities/SecurityLoginAttempt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class SecurityLoginAttempt : ClassBase
+    {
+        public string Email { get; set; } = null!;
+        public string FailureReason { get; set; } = null!;
+        public string IpAddress { get; set; } = null!;
+        public string DeviceInfo { get; set; } = null!;
+
+    }
+}
