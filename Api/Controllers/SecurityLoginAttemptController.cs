565e76baf94dfc9d0ff99ceef9070247f857c793
sebast825 <sebamolina825@gmail.com>
2025-10-21 20:04:49 -0300

    SecurityLoginAttemptController

---
+++ Api/Controllers/SecurityLoginAttemptController.cs
@@ -0,0 +1,24 @@
+﻿using Aplication.Services;
+using Core.Entities;
+using Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecurityLoginAttemptController : Controller
+    {
+        private readonly ISecurityLoginAttemptService _securityLoginAttemptService;
+        public SecurityLoginAttemptController(ISecurityLoginAttemptService securityLoginAttemptService)
+        {
+            _securityLoginAttemptService = securityLoginAttemptService;
+        }
+        [HttpGet]
+        public async Task<List<SecurityLoginAttempt>> GetAll()
+        {
+            return await _securityLoginAttemptService.GetAllAsync();
+         
+        }
+    }
+}
