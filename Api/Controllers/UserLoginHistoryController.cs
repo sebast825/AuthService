6a7eed7c7a30ca59089126c1ec58967d49398ac9
sebast825 <sebamolina825@gmail.com>
2025-10-21 20:20:20 -0300

    UserLoginHistoryController

---
+++ Api/Controllers/UserLoginHistoryController.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserLoginHistoryController : Controller
+    {
+        private readonly IUserLoginHistoryService _userLoginHistoryService;
+        public UserLoginHistoryController(IUserLoginHistoryService userLoginHistoryService)
+        {
+            _userLoginHistoryService = userLoginHistoryService;
+        }
+        [HttpGet("id")]
+        public async Task<List<UserLoginHistory>> GetById(int id)
+        {
+            return await _userLoginHistoryService.GetAllByUserIdAsync(id);
+            
+        }
+    }
+}
