e1a8612a894e571edc3812fec675e4479d467926
sebast825 <sebamolina825@gmail.com>
2025-10-27 14:40:58 -0300

    created bg service for rabbit

---
+++ Infrastructure/EventBus/RabbitMQ/RabbitMQBackgroundService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.EventBus.RabbitMQ
+{
+    public class RabbitMQBackgroundService : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public  RabbitMQBackgroundService(IServiceProvider serviceProvider) {
+            _serviceProvider = serviceProvider; 
+        
+        }
+        //curretly ont using stoppingToke, for production we need to configure this
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var scope  = _serviceProvider.CreateScope();
+            WorkConsumer consumer = new WorkConsumer();
+            await consumer.InitializeAsync();
+
+            // the service remain listening
+            await Task.WhenAll(consumer.ConsumirFallidos(), consumer.ConsumirExitosos());
+                
+        }
+    }
+}
