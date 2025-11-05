63bdf34b4d542a332589a1931783c51666cc4578
sebast825 <sebamolina825@gmail.com>
2025-11-04 21:01:17 -0300

    added ilogger
    added ilogger and messages during execution

--- Infrastructure/EventBus/RabbitMQ/RabbitMQBackgroundService.cs
+++ Infrastructure/EventBus/RabbitMQ/RabbitMQBackgroundService.cs
@@ -1,47 +1,61 @@
 ﻿using Core.Interfaces.EventBus;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Infrastructure.EventBus.RabbitMQ
 {
     public class RabbitMQBackgroundService : BackgroundService
     {
         private IEventConsumer? _consumer;
-        public RabbitMQBackgroundService(IEventConsumer eventConsumer)
+        private readonly ILogger<RabbitMQBackgroundService> _logger;
+        public RabbitMQBackgroundService(IEventConsumer eventConsumer, ILogger<RabbitMQBackgroundService> logger)
         {
             _consumer = eventConsumer;
+            _logger = logger;
 
         }
-        //curretly ont using stoppingToke, for production we need to configure this
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             try
             {
+                _logger.LogInformation("Starting RabbitMQ Background Service. Preparing event consumers.");
                 _consumer.StartConsumingSuccessfulLogins();
                 _consumer.StartConsumingFailedLogins();
+                _logger.LogInformation("Successful and failed login consumers started successfully.");
+
                 await Task.Delay(Timeout.Infinite, stoppingToken);
             }
             catch (OperationCanceledException)
             {
                 // Cancellation requested, exit gracefully
+                _logger.LogInformation("Cancellation requested. Stopping service gracefully.");
+
 
             }
             catch (Exception ex)
             {
+                _logger.LogInformation("Critical error in RabbitMQ Background Service: { Exception.Message}. Restarting or stopping service.");
+
                 throw;
 
             }
         }
         public override async Task StopAsync(CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Stopping RabbitMQ Background Service. Releasing resources.");
+
             if (_consumer != null && _consumer is IAsyncDisposable disposable)
             {
                 await disposable.DisposeAsync(); // use the "disposable" variable from pattern matching
+                _logger.LogInformation("RabbitMQ resources released successfully.");
+
             }
 
             await base.StopAsync(cancellationToken);
