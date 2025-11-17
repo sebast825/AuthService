c2a2c9cf693e18829f75652c2424f092ee39157f
sebast825 <sebamolina825@gmail.com>
2025-11-17 19:48:45 -0300

    created class LoginEventMapper
    added method SecurityLoginAttemptMapper

---
+++ Aplication/Helpers/LoginEventMapper.cs
@@ -0,0 +1,24 @@
+﻿using Core.Constants;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Helpers
+{
+    internal static class LoginEventMapper
+    {
+        public  static SecurityLoginAttempt SecurityLoginAttemptMapper(string email, string reason, string ipAddrress, string deviceInfo)
+        {
+            return new SecurityLoginAttempt()
+            {
+                Email = email,
+                FailureReason = reason,
+                IpAddress = ipAddrress,
+                DeviceInfo = deviceInfo,
+            };
+    }
+    }
+}
