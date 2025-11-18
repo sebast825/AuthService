89672da7610fc56c7b0e8abe4d6ae3561f066bf3
sebast825 <sebamolina825@gmail.com>
2025-11-18 15:05:31 -0300

    fix
    updated test to run with ConcurrentDictionary
    removed _cache
    updated tests
    removed 2 redundant test with the new funtionality:
    - IncrementAttempts_WithExistingAttempts_IncreasesCount
    -IncrementAttempts_WithNotExistingAttempts_IncreasesCount

--- Tests/Services/EmailAttemptsServiceTests.cs
+++ Tests/Services/EmailAttemptsServiceTests.cs
@@ -15,29 +15,38 @@ namespace Tests.Services
     public class EmailAttemptsServiceTests
     {
         private IEmailAttemptsService _emailAttemptsService;
-        private  IMemoryCache _cache;
 
         [TestInitialize]
         public void SetUp()
         {
-            _cache = new MemoryCache(new MemoryCacheOptions());
-            _emailAttemptsService = new EmailAttemptsService(_cache);
+            _emailAttemptsService = new EmailAttemptsService();
         }
 
+        private void AddAttempts(int amount, string email)
+        {
+
+            int i = 0;
+            while (i < amount)
+            {
+                _emailAttemptsService.IncrementAttempts(email);
+                i++;
+            }
+
+        }
         [TestMethod]
         public void IsBlocked_EmailIsBlocked_ReturnTrue()
         {
             string email = "test@gmail.com";
-            _cache.Set(email, 5);
-  
-           bool isBlocked = _emailAttemptsService.EmailIsBlocked(email);
+            AddAttempts(5, email);
+
+            bool isBlocked = _emailAttemptsService.EmailIsBlocked(email);
             Assert.IsTrue(isBlocked);
         }
         [TestMethod]
         public void IsBlocked_EmailIsNotBlocked_ReturnTrue()
         {
             string email = "test@gmail.com";
-            _cache.Set(email, 4);
+            AddAttempts(4, email);
 
             bool isBlocked = _emailAttemptsService.EmailIsBlocked(email);
             Assert.IsFalse(isBlocked);
@@ -47,30 +56,14 @@ namespace Tests.Services
         public void ResetAttempts_WithExistingAttempts_RevemosFromCache()
         {
             string email = "test@gmail.com";
-            int attempts = 4;
-            _cache.Set(email, attempts);
+            int attempts = 6;
+            AddAttempts(attempts, email);
             _emailAttemptsService.ResetAttempts(email);
+            bool isBlocked = _emailAttemptsService.EmailIsBlocked(email);
 
-            Assert.AreEqual(_cache.Get(email), null);
+            Assert.IsFalse(isBlocked);
         }
-        [TestMethod]
-        public void IncrementAttempts_WithExistingAttempts_IncreasesCount()
-        {
-            string email = "test@gmail.com";
-            int attempts = 4;
-            _cache.Set(email, attempts);
-            _emailAttemptsService.IncrementAttempts(email);
 
-            Assert.AreEqual(_cache.Get(email), attempts+1);
-        }
-        [TestMethod]
-        public void IncrementAttempts_WithNotExistingAttempts_IncreasesCount()
-        {
-            string email = "test@gmail.com";
-            int attempts = 1;
-            _emailAttemptsService.IncrementAttempts(email);
 
-            Assert.AreEqual(_cache.Get(email), attempts);
-        }
     }
 }
